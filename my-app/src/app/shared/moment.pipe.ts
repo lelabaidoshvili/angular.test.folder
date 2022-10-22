2498c89a65987e26ff506c89482f86ac60fd9b32
lelabaidoshvili <baidoshvililela3@gmail.com>
2022-10-22 04:08:36 -0700

    my first Angular project

---
+++ my-app/src/app/shared/moment.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from "@angular/core";
+import * as moment from 'moment';
+
+ @Pipe({
+    name: 'moment',
+     pure: false
+
+ })
+
+export class MomentPipe implements PipeTransform{
+    transform(m: moment.Moment, format: string = 'MMM YYYY'):string {
+        return m.format(format)
+
+    }
+}
